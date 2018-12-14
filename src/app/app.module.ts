8f185406eff376592ef1ecc01d46a4abd53b6cc7
Rejwan Ahmed <rejwan.052@gmail.com>
2018-12-14 11:32:11 +0600

    client initial set up & added router link

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { DepartmentsComponent } from './departments/departments.component';
+import { DesignationsComponent } from './designations/designations.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BsDatepickerModule } from "ngx-bootstrap/datepicker";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NotFoundComponent,
+    EmployeesComponent,
+    DepartmentsComponent,
+    DesignationsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BsDatepickerModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
